d088e59eaa285a423982aed2afc4d489d6724b1b
AlysonHenao <159603376+AlysonHenao@users.noreply.github.com>
2026-03-23 15:03:46 -0500

    Revert "feat: Implement authentication and user association for orders and reviews"

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'guest' => RedirectIfAuthenticated::class,
-            'admin' => \App\Http\Middleware\CheckAdmin::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
